d46ade647804a37a5868755da9b999ffd5262d79
Anders Engström <epirealer@gmail.com>
2019-12-05 09:23:25 +0100

    Minor update, day-5

--- src/aoc2019/day_05.clj
+++ src/aoc2019/day_05.clj
@@ -15,20 +15,6 @@
                    1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
                    999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99])
 
-;; Opcodes:
-;; 1 - add values
-;; 2 - mult values
-;; 3 - store INPUT
-;; 4 - write OUTPUT
-;; 5 - jump-if-true [>0, new-pos], else ignore
-;; 6 - jump-if-false [0, new-pos], else ignore
-;; 7 - less-than [a < b, store 1 in c, else 0]
-;; 8 - equals [a == b, store 1 in c, else 0]
-
-;; Param Modes
-;; 0 - mem reference
-;; 1 - direct value
-
 (defn op [n]
   (let [code (rem n 100)
         p1 (quot (rem n 1000) 100)
@@ -39,28 +25,31 @@
 (defn step [{:keys [pos prg input] :as state}]
   (let [[code m1 m2 m3] (op (first (drop pos prg)))]
     (case code
+      ;; 1 - add values
       1 (let [[a b c] (take 3 (drop (inc pos) prg))
               a (if (zero? m1) (get prg a) a)
               b (if (zero? m2) (get prg b) b)]
           (-> state
               (update :pos + 4)
               (update :prg assoc c (+ a b))))
+      ;; 2 - mult values
       2 (let [[a b c] (take 3 (drop (inc pos) prg))
               a (if (zero? m1) (get prg a) a)
               b (if (zero? m2) (get prg b) b)]
           (-> state
               (update :pos + 4)
               (update :prg assoc c (* a b))))
+      ;; 3 - store INPUT
       3 (let [a (first (drop (inc pos) prg))]
-          (println "Reading input")
           (-> state
               (update :pos + 2)
               (update :prg assoc a input)))
+      ;; 4 - write OUTPUT
       4 (let [a (first (drop (inc pos) prg))]
-          (println "Output: " (get prg a))
           (-> state
               (update :outputs conj (get prg a))
               (update :pos + 2)))
+      ;; 5 - jump-if-true [>0, new-pos], else ignore
       5 (let [[a b] (take 2 (drop (inc pos) prg))
               a (if (zero? m1) (get prg a) a)
               b (if (zero? m2) (get prg b) b)]
@@ -68,6 +57,7 @@
             (-> state
                 (assoc :pos b))
             (update state :pos + 3)))
+      ;; 6 - jump-if-false [0, new-pos], else ignore
       6 (let [[a b] (take 2 (drop (inc pos) prg))
               a (if (zero? m1) (get prg a) a)
               b (if (zero? m2) (get prg b) b)]
@@ -75,18 +65,21 @@
             (-> state
                 (assoc :pos b))
             (update state :pos + 3)))
+      ;; 7 - less-than [a < b, store 1 in c, else 0]
       7 (let [[a b c] (take 3 (drop (inc pos) prg))
               a (if (zero? m1) (get prg a) a)
               b (if (zero? m2) (get prg b) b)]
           (-> state
               (update :pos + 4)
               (update :prg assoc c (if (< a b) 1 0))))
+      ;; 8 - equals [a == b, store 1 in c, else 0]
       8 (let [[a b c] (take 3 (drop (inc pos) prg))
               a (if (zero? m1) (get prg a) a)
               b (if (zero? m2) (get prg b) b)]
           (-> state
               (update :pos + 4)
               (update :prg assoc c (if (= a b) 1 0))))
+      ;; End-state
       99 (assoc state :halt? true))))
 
 (defn run [prg input]
@@ -96,3 +89,8 @@
                              :input input
                              :outputs '()})]
     (:outputs (first (drop-while #(not (:halt? %)) steps)))))
+
+(defn run-test []
+  (assert (= 2845163 (first (run input 1))))
+  (assert (= 9436229 (first (run input 5))))
+  "Success!")
