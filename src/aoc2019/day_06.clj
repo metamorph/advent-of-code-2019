891dec02817fa11d119bb9b052931f7a392e7489
Anders Engström <epirealer@gmail.com>
2019-12-06 08:09:35 +0100

    Day 6 (second part)

--- src/aoc2019/day_06.clj
+++ src/aoc2019/day_06.clj
@@ -3,6 +3,7 @@
 
 (def puzzle-input (util/input-lines "input-06.txt"))
 (def test-input (util/input-lines "test-06.txt"))
+(def test-input-2 (util/input-lines "test-06-2.txt"))
 
 (defn input->data [lines]
   (let [re #"([^\)]*)\)([^\)]*)"]
@@ -29,8 +30,20 @@
        (map count)
        (reduce + 0)))
 
+(defn find-common-orbit [p p']
+  (last (last (take-while (partial apply =) (map vector p p')))))
+
+(defn solve-2 [data]
+  (let [my-path  (orbiters data :YOU)
+        san-path (orbiters data :SAN)
+        common   (find-common-orbit my-path san-path)
+        my-dist  (count (take-while #(not= common %) (reverse my-path)))
+        san-dist (count (take-while #(not= common %) (reverse san-path)))]
+    (+ my-dist san-dist)))
+
 (defn verify []
   (assert (= 194721 (solve-1 (input->data puzzle-input))))
+  (assert (= 316 (solve-2 (input->data puzzle-input))))
   "Ok!")
 
 
