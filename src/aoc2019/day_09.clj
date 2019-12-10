09df6bc96a011fe9fe68e6403b1aaa3f85c1b094
Anders Engström <epirealer@gmail.com>
2019-12-10 07:10:31 +0100

    Fixing the intcode program - and day9

--- src/aoc2019/day_09.clj
+++ src/aoc2019/day_09.clj
@@ -1,10 +1,29 @@
 (ns aoc2019.day-09
   (:require [aoc2019.intcodes :as ic]
-            [clojure.core.async :as a]))
+            [clojure.core.async :as a]
+            [clojure.string :as s]
+            [clojure.java.io :as io]))
 
 (def test-1 [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99])
 (def test-2 [1102,34915192,34915192,7,4,7,99,0])
 (def test-3 [104,1125899906842624,99])
+
+(def puzzle-input
+  (let [data (slurp (io/resource "input-09.txt"))]
+    (->> (s/split data #",")
+         (map s/trim)
+         (mapv #(Long/parseLong %)))))
+
+(defn verify []
+  (assert (= [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
+             (ic/run-prg!! [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99] [])))
+  (assert (= 1125899906842624
+             (first (ic/run-prg!! [104,1125899906842624,99] []))))
+  ;; Part 1
+  (assert (= [2316632620] (ic/run-prg!! puzzle-input [1])))
+  ;; Part 2
+  (assert (= [78869] (ic/run-prg!! puzzle-input [2])))
+  "Ok")
 
 
 
--- src/aoc2019/intcodes.clj
+++ src/aoc2019/intcodes.clj
@@ -1,6 +1,10 @@
 (ns aoc2019.intcodes
   (:require [clojure.core.async :as a]))
 
+(def ^:dynamic DEBUG? false)
+
+(defn read-vals [n {:keys [prg pos]}]
+  (map #(get prg % 0) (range pos (+ pos n))))
 
 (defn op
   "Split an integer into op-code and parameter types: `n -> [code p1 p2 p3]`"
@@ -9,6 +13,7 @@
         p1 (quot (rem n 1000) 100)
         p2 (quot (rem n 10000) 1000)
         p3 (quot (rem n 100000) 10000)]
+    (if DEBUG? (printf "op(%d -> %s)\n" n [code p1 p2 p2]))
     [code p1 p2 p3]))
 
 (defn read-pos [{:keys [prg offset]} v mode]
@@ -17,66 +22,87 @@
     1 v
     2 (get prg (+ offset v) 0)))
 
+(defn write-pos [{:keys [prg offset]} v mode]
+  (case mode
+    0 v
+    1 v
+    2 (+ offset v)))
+
+
 (defmulti apply-op (fn [[code & _] _] code))
 
 ;; Addition
 (defmethod apply-op 1 [[_ m1 m2 m3] {:keys [pos prg] :as state}]
-  (let [[a b c] (take 3 (drop (inc pos) prg))]
+  (let [[a b c :as in] (read-vals 3 state)
+        a'             (read-pos state a m1)
+        b'             (read-pos state b m2)
+        c'             (write-pos state c m3)]
     (-> state
-        (update :pos + 4)
-        (update :prg assoc c (+ (read-pos state a m1)
-                                (read-pos state b m2))))))
+        (update :pos + 3)
+        (update :prg assoc c' (+ a' b')))))
 
 ;; Multiply
 (defmethod apply-op 2 [[_ m1 m2 m3] {:keys [pos prg] :as state}]
-  (let [[a b c] (take 3 (drop (inc pos) prg))]
+  (let [[a b c] (read-vals 3 state)
+        a'      (read-pos state a m1)
+        b'      (read-pos state b m2)
+        c'      (write-pos state c m3)]
     (-> state
-        (update :pos + 4)
-        (update :prg assoc c (* (read-pos state a m1)
-                                (read-pos state b m2))))))
+        (update :pos + 3)
+        (update :prg assoc c' (* a' b')))))
 
 ;; Read input and store it
-(defmethod apply-op 3 [_ {:keys [pos prg input-fn] :as state}]
-  (let [a  (first (drop (inc pos) prg))
+(defmethod apply-op 3 [[_ m1 _ _] {:keys [pos prg offset input-fn] :as state}]
+  (let [a  (first (read-vals 1 state))
+        a' (write-pos state a m1)
         in (input-fn)]
+    (assert (not= m1 1))
     (-> state
-        (update :pos + 2)
-        (update :prg assoc a in))))
+        (update :pos + 1)
+        (update :prg assoc a' in))))
 
 ;; Write output
-(defmethod apply-op 4 [_ {:keys [pos prg output-fn] :as state}]
-  (let [a (first (drop (inc pos) prg))
-        v (get prg a 0)]
+(defmethod apply-op 4 [[_ m1 _ _] {:keys [pos prg output-fn] :as state}]
+  (let [a (first (read-vals 1 state))
+        v (read-pos state a m1)]
     (output-fn v)
-    (update state :pos + 2)))
+    (update state :pos + 1)))
 
 ;; Jump if true
 (defmethod apply-op 5 [[_ m1 m2 _] {:keys [pos prg] :as state}]
-  (let [[a b] (take 2 (drop (inc pos) prg))]
-    (if (pos? (read-pos state a m1))
-      (assoc state :pos (read-pos state b m2))
-      (update state :pos + 3))))
+  (let [[a b] (read-vals 2 state)
+        a'    (read-pos state a m1)
+        b'    (read-pos state b m2)]
+    (if (pos? a')
+      (assoc state :pos b')
+      (update state :pos + 2))))
 
 ;; Jump if false
 (defmethod apply-op 6 [[_ m1 m2 _] {:keys [pos prg] :as state}]
-  (let [[a b] (take 2 (drop (inc pos) prg))]
+  (let [[a b] (read-vals 2 state)]
     (if (zero? (read-pos state a m1))
       (assoc state :pos (read-pos state b m2))
-      (update state :pos + 3))))
+      (update state :pos + 2))))
 
 ;; Less-than [a < b, store 1 in c, else 0]
-(defmethod apply-op 7 [[_ m1 m2 _] {:keys [pos prg] :as state}]
-  (let [[a b c] (take 3 (drop (inc pos) prg))]
+(defmethod apply-op 7 [[_ m1 m2 m3] {:keys [pos prg] :as state}]
+  (let [[a b c] (read-vals 3 state)
+        a'      (read-pos state a m1)
+        b'      (read-pos state b m2)
+        c'      (write-pos state c m3)]
     (-> state
-        (update :pos + 4)
-        (update :prg assoc c (if (< (read-pos state a m1) (read-pos state b m2)) 1 0)))))
+        (update :pos + 3)
+        (update :prg assoc c' (if (< a' b') 1 0)))))
 
 ;; Equals [a == b, store 1 in c, else 0]
-(defmethod apply-op 8 [[_ m1 m2 _] {:keys [pos prg] :as state}]
-  (let [[a b c] (take 3 (drop (inc pos) prg))]
+(defmethod apply-op 8 [[_ m1 m2 m3] {:keys [pos prg] :as state}]
+  (let [[a b c] (read-vals 3 state)
+        a'      (read-pos state a m1)
+        b'      (read-pos state b m2)
+        c'      (write-pos state c m3)]
     (-> state
-        (update :pos + 4)
-        (update :prg assoc c (if (= (read-pos state a m1) (read-pos state b m2)) 1 0)))))
+        (update :pos + 3)
+        (update :prg assoc c' (if (= a' b') 1 0)))))
 
 ;; End state
 (defmethod apply-op 99 [_ {:keys [stop-fn] :as state}]
@@ -84,19 +110,20 @@
   (assoc state :halt? true))
 
 ;; Handle relative-base-offset
-(defmethod apply-op 9 [_ {:keys [pos prg offset] :as state}]
-  (let [a (first (drop (inc pos) prg))]
+(defmethod apply-op 9 [[_ m1 _ _] {:keys [pos prg offset] :as state}]
+  (let [a (first (read-vals 1 state))]
     (-> state
-        (update :pos + 2)
-        (update :offset + offset))))
+        (update :pos inc)
+        (update :offset + (read-pos state a m1)))))
 
-(defn step [{:keys [pos prg] :as state}]
-  (let [[code m1 m2 m3 :as pfx] (op (first (drop pos prg)))]
-    (try
-      (apply-op (op (first (drop pos prg))) state)
-      (catch Exception e
-        (println "Error evaluating: " e)
-        (throw (ex-info "Error evaluating" {:state state} e))))))
+(defn step [{:keys [pos prg offset] :as state}]
+  (try
+    (let [[code m1 m2 m3 :as pfx] (op (first (read-vals 1 state)))]
+      (if DEBUG? (printf "Pos: %d, Offset: %d\n" pos offset))
+      (apply-op pfx (update state :pos inc)))
+    (catch Exception e
+      (println "Error evaluating: " e)
+      (throw (ex-info "Error evaluating" {:state state} e)))))
 
 (defn seq!!
   "Returns a (blocking!) lazy sequence read from a channel."
@@ -110,7 +137,7 @@
         output-fn (fn [v] (a/>!! output-ch v))
         input-fn  (fn [] (a/<!! input-ch))
         stop-fn   (fn [] (a/close! output-ch))
-        steps     (iterate step {:prg       prg
+        steps     (iterate step {:prg       (zipmap (iterate inc 0) prg)
                                  :offset    0
                                  :input-fn  input-fn
                                  :output-fn output-fn
@@ -123,3 +150,12 @@
 (defn run-prg!! [prg inputs]
   (let [out-ch (run-prg prg (a/to-chan inputs))]
     (seq!! out-ch)))
+
+(defn verify []
+  (assert (= [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
+             (run-prg!! [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99] [])))
+  (assert (= [1125899906842624] (run-prg!! [104,1125899906842624,99] [])))
+  (assert (= [999] (run-prg!! [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99] [5])))
+  (assert (= [1000] (run-prg!! [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99] [8])))
+  (assert (= [1001] (run-prg!! [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99] [12])))
+  "OK")
